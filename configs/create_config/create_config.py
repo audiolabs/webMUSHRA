42b425649d425790983f6d06e32e50783d27b18a
hell-rh <helena.raymond-hayling@bbc.co.uk>
2024-07-23 11:40:09 +0100

    making groups

--- configs/create_config/create_config.py
+++ configs/create_config/create_config.py
@@ -1,23 +1,29 @@
 import os
 import glob
+import numpy as np
+import pandas as pd
 
 # Get the list of .wav files
 wav_files = glob.glob("../../stimuli/*.wav")
 
-# Extract and sort unique prefixes
-stimuli_list = sorted(
-    {"-".join(os.path.basename(file).split("-")[0:2]) for file in wav_files}
-)
-print(stimuli_list)
-assert len(stimuli_list) == 33
+clip_names_csv = "../../../utils/clip_names_map.csv"
 
 
-# Read the YAML file as text
-with open("before.yaml", "r") as file:
-    yaml_text = file.read()
+# Two groups
+df = pd.read_csv(clip_names_csv, index_col=0)
+group1 = df[df.group == 1]
+group2 = df[df.group == 2]
+training = df[df.group == 0]
 
-training_clip = "HDM-02"
-yaml_text += f"""
+
+for group in [group1, group2]:
+    # Read the YAML file as text
+    with open("before.yaml", "r") as file:
+        yaml_text = file.read()
+
+    training_clip = training.new.values[0]
+
+    yaml_text += f"""
     - type: mushra
       id: training
       name: MUSHRA
@@ -31,37 +37,36 @@ yaml_text += f"""
           C2: stimuli/{training_clip}-remix-orig-hf-n.wav
           C3: stimuli/{training_clip}-remix-bss-htdft-hf-n.wav
           C4: stimuli/{training_clip}-remix-bss-cfk-hf-n.wav
-"""
+    """
 
-yaml_text += """    -
-      - random"""
+    yaml_text += """
+    -
+        - random"""
 
-for i, clipname in enumerate(stimuli_list[0:1]):
-    print(clipname)
-    experiment_page = f"""
-      - type: mushra
-        id: trial_{i+1}_{clipname}
-        name: Clip {i+1}
-        content: How does the speech clarity of these clips compare to the reference clip?
-        reference: stimuli/{clipname}-mix.wav
-        stimuli:
-            C1: stimuli/{clipname}-mix-hf-n.wav
-            C2: stimuli/{clipname}-remix-orig-hf-n.wav
-            C3: stimuli/{clipname}-remix-bss-htdft-hf-n.wav
-            C4: stimuli/{clipname}-remix-bss-cfk-hf-n.wav
-        enableLooping: true
-        randomize: true
-"""
-    # break
-    yaml_text += experiment_page
+    for i, clipname in enumerate(group.new):
+        print(f"{i} Group{np.unique(group.group)[0]} {clipname}")
+        experiment_page = f"""
+        - type: mushra
+            id: trial_{i+1}_{clipname}
+            name: Clip {i+1}
+            content: How does the speech clarity of these clips compare to the reference ?
+            reference: stimuli/{clipname}-mix.wav
+            stimuli:
+                C1: stimuli/{clipname}-mix-hf-n.wav
+                C2: stimuli/{clipname}-remix-orig-hf-n.wav
+                C3: stimuli/{clipname}-remix-bss-htdft-hf-n.wav
+                C4: stimuli/{clipname}-remix-bss-cfk-hf-n.wav
+            enableLooping: true
+            randomize: true
+    """
+        # break
+        yaml_text += experiment_page
 
+    # Read the YAML file as text
+    with open("after.yaml", "r") as file:
+        after_text = file.read()
 
-# Read the YAML file as text
-with open("after.yaml", "r") as file:
-    after_text = file.read()
+    yaml_text += after_text
 
-
-yaml_text += after_text
-
-with open("../hell.yaml", "w") as file:
-    file.write(yaml_text)
+    with open(f"../group_{np.unique(group.group)[0]}.yaml", "w") as file:
+        file.write(yaml_text)
